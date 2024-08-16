8eaee373177fa3b7cdada05304e1ac47e030c35b
Jess Todd <jessicacktodd@gmail.com>
2024-08-16 16:23:14 +0100

    all pages but not converted to html yet

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<str:title>", views.entry, name="entry"),
     path("search/", views.search, name="search"),
+    path("create/", views.create, name="create"),
+    path("<str:title>/edit/", views.edit, name="edit"),
+    path("random/", views.random_page, name="random")
 ]
 
 
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.http import Http404
+import random
 
 from . import util
 
@@ -7,7 +9,6 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
 
 
 def entry(request, title):
@@ -44,3 +45,45 @@ def search(request):
         "entries": [],
         "query": query
     })
+
+
+def create(request):
+    if request.method == "POST":
+        title = request.POST.get('title', '')
+        content = request.POST.get('content', '')
+
+        if util.get_entry(title) is not None:
+            return render(request, "encyclopedia/create.html", {
+                "error": "An entry with this title already exists.",
+                "title": title,
+                "content": content
+            })
+        
+        util.save_entry(title, content)
+        
+        return redirect('entry', title=title)
+    else:
+        return render(request, "encyclopedia/create.html")
+
+
+def edit(request, title):
+    if request.method == "POST":
+        updated_content = request.POST.get("content")
+        util.save_entry(title, updated_content)
+        return redirect('entry', title=title)
+    
+    content = util.get_entry(title)
+    if content is None:
+        raise Http404("The requested page was not found.")
+    
+    return render(request, "encyclopedia/edit.html", {
+        "title": title,
+        "content": content
+    })
+    
+def random_page(request):
+    entries = util.list_entries()
+
+    random_entry = random.choice(entries)
+
+    return redirect('entry', title=random_entry) 
